a6b144bebf79af4cab1af3e3366d0666b306ed9d
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-03-26 15:49:31 +1100

    Update HashtagList to map over an array of hashtag strings

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react";
 import Container from "./layout/Container";
 import Footer from "./layout/Footer";
-import HashtagList from "./HashtagList";
+import HashtagList from "./hashtag/HashtagList";
 import { TFeedbackItem } from "../lib/types";
 
 function App() {
   const [feedbackItems, setFeedbackItems] = useState<TFeedbackItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+
+  const companyList = feedbackItems
+    .map((item) => item.company)
+    .filter((company, index, array) => {
+      return array.indexOf(company) === index;
+    });
 
   const handleAddToList = async (text: string) => {
     const companyName = text
@@ -75,7 +81,7 @@ function App() {
         handleAddToList={handleAddToList}
       />
 
-      <HashtagList />
+      <HashtagList companyList={companyList} />
     </div>
   );
 }
