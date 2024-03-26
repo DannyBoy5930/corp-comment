9670b56e18e3cd20bcfab249b6da69f5a13f1e57
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-03-26 14:40:52 +1100

    Remove fake feedbackItem data that was used for testing

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -2,23 +2,6 @@ import FeedbackItem from "./FeedbackItem";
 import Spinner from "./Spinner";
 import ErrorMessage from "./ErrorMessage";
 
-const feedbackItems = [
-  {
-    upvoteCount: 593,
-    badgeLetter: "B",
-    companyName: "ByteGrad",
-    text: "test test test",
-    daysAgo: 4,
-  },
-  {
-    upvoteCount: 124,
-    badgeLetter: "N",
-    companyName: "Nike",
-    text: "Nike is a great company",
-    daysAgo: 2,
-  },
-];
-
 export default function FeedbackList({
   feedbackItems,
   isLoading,
